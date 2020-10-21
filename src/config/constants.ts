52f2686f7e46e8c47d4301f1068cf3d22fbe8080
robertbrada <bert.blader@gmail.com>
2020-10-21 21:14:56 +0200

    feat: telegram link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const GITHUB_LINK = 'https://github.com/croco-finance';
-export const TELEGRAM_LINK = '#';
+export const TELEGRAM_LINK = 'https://t.me/crocofinance';
 export const TWITTER_LINK = '#';
 
 // 'http://127.0.0.1:5000/v1/cumulative/<address>';
