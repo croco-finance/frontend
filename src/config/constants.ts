50a4f5dcaa70830c6d6d1cea7b8359d58b0dd2f6
robertbrada <bert.blader@gmail.com>
2020-10-21 21:26:43 +0200

    faet: added Twitter link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const GITHUB_LINK = 'https://github.com/croco-finance';
 export const TELEGRAM_LINK = 'https://t.me/crocofinance';
-export const TWITTER_LINK = '#';
+export const TWITTER_LINK = 'https://twitter.com/CrocoFinance';
 
 // 'http://127.0.0.1:5000/v1/cumulative/<address>';
 export const SERVER_STATS_ENDPOINT = 'http://127.0.0.1:5000/api/v1/cumulative/';
