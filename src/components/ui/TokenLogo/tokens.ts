3d2bb956a42b2922b03dd1c0aaa549e008d919e0
robertbrada <bert.blader@gmail.com>
2020-10-21 10:18:08 +0200

    chore: added more token logos

--- src/components/ui/TokenLogo/tokens.ts
+++ src/components/ui/TokenLogo/tokens.ts
@@ -1,19 +1,22 @@
 /* eslint-disable global-require */
 export const TOKENS: any = {
+    aave: require('../../../data/images/tokens/aave.svg'),
     bal: require('../../../data/images/tokens/bal.svg'),
     comp: require('../../../data/images/tokens/comp.svg'),
     dai: require('../../../data/images/tokens/dai.svg'),
-    weth: require('../../../data/images/tokens/weth.svg'),
+    keep: require('../../../data/images/tokens/keep.svg'),
     lend: require('../../../data/images/tokens/lend.svg'),
-    aave: require('../../../data/images/tokens/lend.svg'),
     link: require('../../../data/images/tokens/link.svg'),
+    mkr: require('../../../data/images/tokens/mkr.svg'),
+    musd: require('../../../data/images/tokens/musd.svg'),
     uni: require('../../../data/images/tokens/uni.svg'),
     usdc: require('../../../data/images/tokens/usdc.svg'),
     usdt: require('../../../data/images/tokens/usdt.svg'),
     wbtc: require('../../../data/images/tokens/wbtc.svg'),
     yfi: require('../../../data/images/tokens/yfi.svg'),
-    keep: require('../../../data/images/tokens/keep.svg'),
     snx: require('../../../data/images/tokens/snx.svg'),
+    weth: require('../../../data/images/tokens/weth.svg'),
+    // unknown token
     unknown: require('../../../data/images/tokens/unknown.svg'),
     // exchanges
     uniswap: require('../../../data/images/exchanges/uniswap.svg'),
