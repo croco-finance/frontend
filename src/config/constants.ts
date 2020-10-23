525b7e98576abf7275e26b34336f7130cd5687b8
benesjan <janbenes1234@gmail.com>
2020-10-23 09:00:03 +0200

    Encrypted server end point

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,6 @@ export const GITHUB_LINK = 'https://github.com/croco-finance';
 export const TELEGRAM_LINK = 'https://t.me/crocofinance';
 export const TWITTER_LINK = 'https://twitter.com/CrocoFinance';
 
-export const SERVER_STATS_ENDPOINT = 'http://127.0.0.1:5000/api/v1/cumulative/';
+export const SERVER_STATS_ENDPOINT = 'https://croco.finance/api/v1/cumulative/';
 
 export const PORTIS_DAPP_KEY = 'a73f6025-1669-49cf-9880-a81d3de67e7f';
