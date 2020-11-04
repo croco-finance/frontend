c95851e05f86cd723bb43446a30efa15699afcd1
robertbrada <bert.blader@gmail.com>
2020-11-04 11:42:27 +0000

    feat: GA tracking using script in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import { Route, Switch } from 'react-router-dom';
 import Dashboard from './views/dashboard';
 import Simulator from './views/simulator';
 import LandingPage from './views/landing-page';
-import './App.css';
-import { PageView, initGA } from './config/analytics';
 import { RouteComponentProps, withRouter } from 'react-router';
+import ReactGA from 'react-ga';
+import { GOOGLE_ANALYTICS_TRACKING_ID } from './config/constants';
+import './App.css';
+
+// this is still not working properly. I am using GA script in index.html to track basic traffic
+ReactGA.initialize(GOOGLE_ANALYTICS_TRACKING_ID);
 
 const App = (props: RouteComponentProps<any>) => {
     useEffect(() => {
         // enable Google Analytics
-        initGA();
-        PageView();
+        ReactGA.pageview(window.location.pathname + window.location.search);
 
         // check if there is any address stored in browser local storage
-        const addressLocalStorage = localStorage.getItem('address');
+        // local storage is not accessible in discreet mode
+        try {
+            const addressLocalStorage = localStorage.getItem('address');
 
-        // if there is some valid address, go directly to dashboard so that the user doesn't have to paste his address again
-        if (addressLocalStorage) {
-            props.history.push({
-                pathname: `/dashboard/${addressLocalStorage}`,
-            });
+            // if there is some valid address, go directly to dashboard so that the user doesn't have to paste his address again
+            if (addressLocalStorage) {
+                props.history.push({
+                    pathname: `/dashboard/${addressLocalStorage}`,
+                });
+            }
+        } catch (e) {
+            console.log('Error while trying to access local storage');
         }
     }, []);
 
