c14f8e62a3d93cb58246fc7267d14a0b4460fe12
robertbrada <bert.blader@gmail.com>
2020-12-02 22:21:30 +0100

    performance improved, code simplified, est. graph

---
+++ src/views/simulator/components/RightContainer/Strategies/Graph/index.tsx
@@ -0,0 +1,169 @@
+import styled, { css } from 'styled-components';
+import { colors, variables } from '@config';
+import { formatUtils } from '@utils';
+import React, { PureComponent } from 'react';
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Label,
+    ResponsiveContainer,
+    ReferenceDot,
+    ReferenceLine,
+    Legend,
+} from 'recharts';
+
+import CustomTooltip from './CustomTooltip';
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'Usd',
+    minimumFractionDigits: 2,
+});
+
+const getYAxisMaxValue = value => {
+    return Math.round(value / 1000) * 1000 + 1000;
+};
+
+const LegendItem = styled.span`
+    font-size: ${variables.FONT_SIZE.NORMAL};
+`;
+
+interface Props {
+    height?: number;
+    referenceX?: number;
+    referenceY?: number;
+    data?: any;
+    maxPossibleValue: number;
+}
+
+interface State {
+    highlightedAreaId: string | null;
+}
+
+class Graph extends PureComponent<Props, State> {
+    constructor(props) {
+        super(props);
+    }
+
+    renderColorfulLegendText(value, entry) {
+        let legendText = '';
+        switch (value) {
+            case 'poolValue':
+                legendText = 'Pool value';
+                break;
+            case 'hodlValue':
+                legendText = 'HODL value';
+                break;
+            case 'ethHodlValue':
+                legendText = 'ETH HODL value';
+        }
+
+        return <LegendItem style={{ color: colors.FONT_LIGHT }}>{legendText}</LegendItem>;
+    }
+
+    valueToUsd(value) {
+        return formatter.format(value);
+    }
+    render() {
+        const { data, maxPossibleValue } = this.props;
+
+        return (
+            <ResponsiveContainer width="100%" height={300}>
+                <AreaChart
+                    width={800}
+                    height={290}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 55,
+                        bottom: 10,
+                        left: 55,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="2 2" />
+
+                    <Tooltip
+                        cursor={{ stroke: '#4366b1ff', strokeWidth: 1 }}
+                        content={<CustomTooltip />}
+                    />
+                    <Legend
+                        iconType="plainline"
+                        wrapperStyle={{
+                            paddingLeft: '50px',
+                            paddingTop: '10px',
+                        }}
+                        formatter={this.renderColorfulLegendText}
+                    />
+
+                    <Area
+                        key={'poolValue'}
+                        isAnimationActive={false}
+                        dataKey={'poolValue'}
+                        strokeWidth={1}
+                        fillOpacity={0.7}
+                        fill={colors.GRAPH_1_DARK}
+                        stroke={colors.GRAPH_1_DARK}
+                    />
+                    <Area
+                        key={'hodlValue'}
+                        isAnimationActive={false}
+                        dataKey={'hodlValue'}
+                        strokeWidth={2}
+                        fillOpacity={0}
+                        fill={colors.BLUE}
+                        stroke={'#a600ffff'}
+                        // stroke={colors.BLUE}
+                        strokeDasharray="4 3"
+                    />
+                    <Area
+                        key={'ethHodlValue'}
+                        isAnimationActive={false}
+                        dataKey={'ethHodlValue'}
+                        strokeWidth={2}
+                        fillOpacity={0}
+                        fill={colors.BLUE}
+                        stroke={'#0100d2'}
+                        // stroke={colors.BLUE}
+                        strokeDasharray="4 3"
+                    />
+
+                    <XAxis
+                        dataKey="name"
+                        tick={{
+                            fontSize: variables.FONT_SIZE.NORMAL,
+                            transform: 'translate(0, 12)',
+                        }}
+                        stroke={colors.FONT_MEDIUM}
+                        // padding={{ left: 2 }}
+                    >
+                        {/* <Label
+                            value="Date"
+                            position="bottom"
+                            offset={15}
+                            style={{
+                                textAnchor: 'middle',
+                                fontSize: variables.FONT_SIZE.NORMAL,
+                                fill: colors.FONT_MEDIUM,
+                            }}
+                        /> */}
+                    </XAxis>
+                    <YAxis
+                        tick={{
+                            fontSize: variables.FONT_SIZE.SMALL,
+                            transform: 'translate(-8, 0)',
+                        }}
+                        domain={[0, getYAxisMaxValue(maxPossibleValue)]}
+                        stroke={colors.FONT_MEDIUM}
+                        tickFormatter={this.valueToUsd}
+                    ></YAxis>
+                </AreaChart>
+            </ResponsiveContainer>
+        );
+    }
+}
+
+export default Graph;
