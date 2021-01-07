ddcf63a0341ed158ab93e8c33499d1735419b51e
robertbrada <bert.blader@gmail.com>
2021-01-07 20:21:21 +0100

    feat: fee chart stats

--- src/views/dashboard/components/RightContainer/DailyFees/index.tsx
+++ src/views/dashboard/components/RightContainer/DailyFees/index.tsx
@@ -1,9 +1,9 @@
-import { Spinner } from '@components/ui';
+import { Spinner, GrayBox, FiatValue } from '@components/ui';
 import { variables } from '@config';
 import { useTheme } from '@hooks';
 import { useSelector } from '@reducers';
 import { PoolItem } from '@types';
-import { formatUtils, graphUtils } from '@utils';
+import { formatUtils, graphUtils, mathUtils } from '@utils';
 import React from 'react';
 import styled from 'styled-components';
 import FeesGraph from './Graph';
@@ -28,6 +28,31 @@ const GraphHeadline = styled.div`
     color: ${props => props.theme.FONT_MEDIUM};
     font-size: ${variables.FONT_SIZE.NORMAL};
 `;
+const StyledGrayBox = styled(GrayBox)`
+    margin: 30px 15px 0px 20px;
+`;
+const FeeStatsBox = styled.div`
+    display: flex;
+    width: 100%;
+`;
+
+const Stat = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 33%;
+    color: ${props => props.theme.FONT_MEDIUM};
+    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+
+    @media (max-width: ${variables.SCREEN_SIZE.MD}) {
+        font-size: ${variables.FONT_SIZE.SMALL};
+    }
+`;
+
+const FeeStatHeadline = styled.div`
+    color: ${props => props.theme.FONT_LIGHT};
+    margin-right: 8px;
+`;
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     selectedPool: PoolItem | undefined;
     graphHeight?: number;
@@ -38,12 +63,14 @@ const DailyFees = ({ selectedPool, graphHeight = 380 }: Props) => {
     const theme: any = useTheme();
 
     let data: any = null;
+    let feesUsd: number[] | null = null;
     if (selectedPool) {
         // hide fees chart for inactive pools
         if (!selectedPool.isActive) return null;
 
         if (selectedPool.dailyStats) {
             data = graphUtils.getDailyGraphData(selectedPool.dailyStats, selectedPool.tokenSymbols);
+            feesUsd = selectedPool.dailyStats.feesUsd;
         }
     }
 
@@ -60,7 +87,35 @@ const DailyFees = ({ selectedPool, graphHeight = 380 }: Props) => {
             ) : (
                 <>
                     <GraphHeadline>{`Daily fees earned since ${earnedSinceText}`}</GraphHeadline>
-                    {data && <FeesGraph data={data} theme={theme} height={graphHeight}></FeesGraph>}
+                    {data && feesUsd ? (
+                        <>
+                            <FeesGraph
+                                data={data}
+                                averageFees={mathUtils.getArrayAverage(feesUsd)}
+                                theme={theme}
+                                height={graphHeight}
+                            ></FeesGraph>
+                            <StyledGrayBox
+                                backgroundColor={theme.BACKGROUND}
+                                padding={[10, 10, 10, 10]}
+                            >
+                                <FeeStatsBox>
+                                    <Stat>
+                                        <FeeStatHeadline>Min:</FeeStatHeadline>
+                                        <FiatValue value={Math.min(...feesUsd)} />
+                                    </Stat>
+                                    <Stat>
+                                        <FeeStatHeadline>Max:</FeeStatHeadline>
+                                        <FiatValue value={Math.max(...feesUsd)} />
+                                    </Stat>
+                                    <Stat>
+                                        <FeeStatHeadline>Avg:</FeeStatHeadline>
+                                        <FiatValue value={mathUtils.getArrayAverage(feesUsd)} />
+                                    </Stat>
+                                </FeeStatsBox>
+                            </StyledGrayBox>
+                        </>
+                    ) : null}
                 </>
             )}
         </Wrapper>
