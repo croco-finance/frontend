c1909b119ccdcfcb49395039ab24b33c225bdabc
benesjan <janbenes1234@gmail.com>
2021-02-26 12:35:40 -0600

    fix: removing 0 snaps after if they follow non-zero stake snap

--- src/utils/firebase-loader.ts
+++ src/utils/firebase-loader.ts
@@ -29,6 +29,7 @@ async function getSnaps(address: string): Promise<SnapStructure | null> {
 
         const dayIds: { [key in Exchange]?: number } = {};
         for (const [poolId, poolSnaps] of Object.entries(snaps)) {
+            filter0SnapsAfterNoUnstake(poolSnaps);
             filter0SameBlockSnaps(poolSnaps);
             let lastSnap = poolSnaps[poolSnaps.length - 1];
             if (lastSnap.liquidityTokenBalance !== 0) {
@@ -129,6 +130,16 @@ function parseSnap(snap: any): Snap {
             ? snap['idWithinStakingContract']
             : null,
     };
+}
+
+function filter0SnapsAfterNoUnstake(snaps: Snap[]) {
+    for (let i = 0; i < snaps.length - 1; i++) {
+        if (snaps[i].stakingService !== null && snaps[i].liquidityTokenBalance !== 0 && snaps[i + 1].stakingService === null && snaps[i + 1].liquidityTokenBalance === 0) {
+            // delete snap if there is a 0 snap after staked snap without unstaking (sometimes 0 snaps are created in the subgraph)
+            snaps.splice(i + 1, 1);
+            i--;
+        }
+    }
 }
 
 function filter0SameBlockSnaps(snaps: Snap[]) {
